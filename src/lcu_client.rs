9d754145ad4f34e234ca970a003e1373fa57ea4c
uflowie <bananasawsm@gmail.com>
2024-08-13 01:05:49 +0200

    adds more error handling to lcu client

--- src/lcu_client.rs
+++ src/lcu_client.rs
@@ -1,11 +1,14 @@
 use base64::{engine::general_purpose, Engine};
-use futures::SinkExt;
+use futures::{SinkExt, Stream};
 use native_tls::TlsConnector;
 use serde::{de::IgnoredAny, Deserialize};
 use sysinfo::System;
-use tokio::sync::watch;
+use tokio::{net::TcpStream, sync::watch};
 use tokio_stream::StreamExt;
-use tokio_tungstenite::tungstenite::{self, client::IntoClientRequest};
+use tokio_tungstenite::{
+    tungstenite::{self, client::IntoClientRequest, Error},
+    MaybeTlsStream, WebSocketStream,
+};
 
 #[derive(Clone, Debug)]
 pub enum LCUState {
@@ -24,87 +27,60 @@ pub async fn run_lcu_client() -> watch::Receiver<LCUState> {
         let mut sys = System::new_all();
 
         loop {
-            let (token, port) = get_lcu_process_data(&mut sys).await;
+            tx.send(LCUState::NotConnected).unwrap();
 
-            let token = general_purpose::STANDARD.encode(format!("riot:{token}"));
-            let auth = format!("Basic {}", token);
+            let (token, port) = get_token_and_port(&mut sys).await;
 
-            println!("auth token: {}", auth);
+            let socket = match get_ws(token, port).await {
+                Ok(socket) => socket,
+                Err(e) => {
+                    println!("Failed to connect to websocket: {}", e);
+                    continue;
+                }
+            };
 
-            let ws_addr = format!("wss://127.0.0.1:{port}");
-            let mut request = ws_addr.into_client_request().unwrap();
-            let headers = request.headers_mut();
-            headers.insert("Authorization", auth.parse().unwrap());
+            let mut event_stream = match get_resource_stream(socket).await {
+                Ok(event_stream) => event_stream,
+                Err(e) => {
+                    println!("Failed to get event stream: {}", e);
+                    tx.send(LCUState::NotConnected).unwrap();
+                    continue;
+                }
+            };
 
-            let config = TlsConnector::builder()
-                .danger_accept_invalid_certs(true)
-                .build()
-                .unwrap();
+            tx.send(LCUState::Connected).unwrap();
 
-            // create tls connector
-            let connector = tokio_tungstenite::Connector::NativeTls(config);
-
-            let (mut socket, _) = tokio_tungstenite::connect_async_tls_with_config(
-                request,
-                None,
-                false,
-                Some(connector),
-            )
-            .await
-            .expect("Failed to connect to websocket");
-
-            // subscribe to json api events
-            socket
-                .send(tungstenite::Message::Text(
-                    "[5, \"OnJsonApiEvent\"]".to_string(),
-                ))
-                .await
-                .expect("Failed to send initial message to websocket");
-
-            // wait on websocket for new events
-            while let Some(Ok(msg)) = socket.next().await {
-                if let tungstenite::Message::Text(msg) = msg {
-                    println!("Received message: {}\n", msg);
-
-                    // parse msg
-                    if let Ok(LCUEvent(_, _, LCUEventData { data: msg })) =
-                        serde_json::from_str::<LCUEvent>(&msg)
-                    {
-                        match msg {
-                            LCUResource::Summoner(summoner) => {
-                                let champion_name = summoner.champion_name;
-                                if !summoner.is_self
-                                    || champion_name == ""
-                                    || champion_name == last_champ
-                                {
-                                    continue;
-                                }
-                                last_champ = champion_name;
-                                if last_game_mode != "" {
-                                    tx.send(LCUState::Playing {
-                                        champion: last_champ.clone(),
-                                        game_mode: last_game_mode.clone(),
-                                    })
-                                    .unwrap();
-                                }
-                            }
-                            LCUResource::Gameflow(gameflow) => {
-                                let queue_name = gameflow.game_data.queue.game_mode;
-                                if queue_name == last_game_mode || queue_name == "" {
-                                    continue;
-                                }
-                                last_game_mode = queue_name;
-                                if last_champ != "" {
-                                    tx.send(LCUState::Playing {
-                                        champion: last_champ.clone(),
-                                        game_mode: last_game_mode.clone(),
-                                    })
-                                    .unwrap();
-                                }
-                            }
-                            LCUResource::Other(_) => {}
+            while let Some(resource) = event_stream.next().await {
+                match resource {
+                    LCUResource::Summoner(summoner) => {
+                        let champion_name = summoner.champion_name;
+                        if !summoner.is_self || champion_name == "" || champion_name == last_champ {
+                            continue;
+                        }
+                        last_champ = champion_name;
+                        if last_game_mode != "" {
+                            tx.send(LCUState::Playing {
+                                champion: last_champ.clone(),
+                                game_mode: last_game_mode.clone(),
+                            })
+                            .unwrap();
                         }
                     }
+                    LCUResource::Gameflow(gameflow) => {
+                        let game_mode = gameflow.game_data.queue.game_mode;
+                        if game_mode == last_game_mode || game_mode == "" {
+                            continue;
+                        }
+                        last_game_mode = game_mode;
+                        if last_champ != "" {
+                            tx.send(LCUState::Playing {
+                                champion: last_champ.clone(),
+                                game_mode: last_game_mode.clone(),
+                            })
+                            .unwrap();
+                        }
+                    }
+                    LCUResource::Other(_) => {}
                 }
             }
         }
@@ -113,27 +89,20 @@ pub async fn run_lcu_client() -> watch::Receiver<LCUState> {
     rx
 }
 
-async fn get_lcu_process_data(sys: &mut System) -> (String, String) {
-    // get lcu port and remoting token
+async fn get_token_and_port(sys: &mut System) -> (String, String) {
     loop {
         sys.refresh_all();
 
         let mut token = None;
         let mut port = None;
 
-        for (_, process) in sys.processes() {
-            if process.name() != "LeagueClientUx.exe" {
-                continue;
-            }
-
+        for process in sys.processes_by_name("LeagueClientUx.exe") {
             for arg in process.cmd() {
-                if arg.starts_with("--remoting-auth-token") {
-                    println!("Found auth token: {}", arg);
-                    token = Some(arg.split("=").collect::<Vec<&str>>()[1].to_string());
+                if let Some(t) = arg.strip_prefix("--remoting-auth-token=") {
+                    token = Some(t.to_string());
                 }
-                if arg.starts_with("--app-port") {
-                    println!("Found app port: {}", arg);
-                    port = Some(arg.split("=").collect::<Vec<&str>>()[1].to_string());
+                if let Some(p) = arg.strip_prefix("--app-port=") {
+                    port = Some(p.to_string());
                 }
             }
         }
@@ -145,6 +114,52 @@ async fn get_lcu_process_data(sys: &mut System) -> (String, String) {
         println!("Couldn't find auth token, sleeping for 1 second");
         tokio::time::sleep(std::time::Duration::from_secs(1)).await;
     }
+}
+
+async fn get_ws(
+    token: String,
+    port: String,
+) -> anyhow::Result<WebSocketStream<MaybeTlsStream<TcpStream>>> {
+    let token = general_purpose::STANDARD.encode(format!("riot:{token}"));
+    let auth = format!("Basic {}", token);
+
+    println!("auth token: {}", auth);
+
+    let ws_addr = format!("wss://127.0.0.1:{port}");
+    let mut request = ws_addr.into_client_request()?;
+    let headers = request.headers_mut();
+    headers.insert("Authorization", auth.parse()?);
+
+    let config = TlsConnector::builder()
+        .danger_accept_invalid_certs(true)
+        .build()?;
+
+    // create tls connector
+    let connector = tokio_tungstenite::Connector::NativeTls(config);
+
+    let (socket, _) =
+        tokio_tungstenite::connect_async_tls_with_config(request, None, false, Some(connector))
+            .await?;
+
+    Ok(socket)
+}
+
+async fn get_resource_stream(
+    mut socket: WebSocketStream<MaybeTlsStream<TcpStream>>,
+) -> Result<impl Stream<Item = LCUResource>, Error> {
+    // subscribe to json api events
+    socket
+        .send(tungstenite::Message::Text(
+            "[5, \"OnJsonApiEvent\"]".to_string(),
+        ))
+        .await?;
+
+    Ok(socket.filter_map(|msg| match msg {
+        Ok(tungstenite::Message::Text(msg)) => serde_json::from_str::<LCUEvent>(&msg)
+            .ok()
+            .map(|event| event.2.data),
+        _ => None,
+    }))
 }
 
 #[derive(Deserialize)]
